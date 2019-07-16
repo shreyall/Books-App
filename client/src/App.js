a4cbcf98a624b4b1df84cfa8234cf458e108624d
Shreyal Shah <shreyalshah@live.com>
2019-07-16 11:38:29 -0400

    Added React client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BookList from "./components/BookList";
+function App() {
+  return (
+    <div id="main">
+      <h1> Shreyal's Reading List </h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function BookList() {
+  return (
+    <div id="main">
+      <ul id="book-list">
+        <li> book name </li>
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;
