9aa4f1aa183b859723749dcbc9271409a028b432
Shreyal Shah <shreyalshah@live.com>
2019-07-16 12:41:14 -0400

    feat(BookList); gave BookList component query access

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+// components
 import BookList from "./components/BookList";
+
+// apollo client set up
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
 function App() {
   return (
-    <div id="main">
-      <h1> Shreyal's Reading List </h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1> Shreyal's Reading List </h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
 
-function BookList() {
-  return (
-    <div id="main">
-      <ul id="book-list">
-        <li> book name </li>
-      </ul>
-    </div>
-  );
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+class BookList extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div id="main">
+        <ul id="book-list">
+          <li> book name </li>
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
